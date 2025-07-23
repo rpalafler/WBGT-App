407c0a70317b0e94d32f6e74323d274896c51d35
miguebmv <miguelangelbravo2000@gmail.com>
2025-07-23 12:14:57 +0200

    connecting frontend with ryans server

--- frontend/src/services/WBGTDataService.js
+++ frontend/src/services/WBGTDataService.js
@@ -1,7 +1,7 @@
 import pako from "pako";
 
-const API_URL = "http://127.0.0.1:8000/api";
-
+// const API_URL = "http://127.0.0.1:8000/api"; // my localhost
+const API_URL = "https://4dvdrhi.sdsu.edu/";
 export async function fetchWBGTData(datetimeStr, forecastHour) {
   const url = `${API_URL}/wbgt/${datetimeStr}/${forecastHour}`;
 
